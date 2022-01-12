a59807809ff74169f8e0a4b592befffafd9218c4
Jon Zakay <JonZakay@aol.com>
2022-01-12 21:25:37 +0000

    Reworked workflows + updated package details

--- setup.py
+++ setup.py
@@ -2,21 +2,21 @@ from setuptools import setup, find_packages
 from os.path import join, dirname
 
 setup(
-    name='hm_pyhelper',
+    name='forked_hm-diag',
     version='0.12.5',
-    author="Nebra Ltd",
-    author_email="support@nebra.com",
+    author="Jon",
+    author_email="JonZakay@aol.com",
     description="Helium Python Helper",
     long_description=open(join(dirname(__file__), 'README.md')).read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/NebraLtd/hm-pyhelper",
+    url="https://github.com/jonzky/hm-pyhelper",
     install_requires=[
         'requests>=2.26.0',
         'jsonrpcclient==3.3.6',
         'retry==0.9.2'
     ],
     project_urls={
-        "Bug Tracker": "https://github.com/NebraLtd/hm-pyhelper/issues",
+        "Bug Tracker": "https://github.com/jonzky/hm-pyhelper/issues",
     },
     packages=find_packages(),
     include_package_data=True
